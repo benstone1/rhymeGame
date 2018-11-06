08d6f2a3634d0174a7cacfdcba4ae8f9403a02e6
Ben Stone <bstone@shopkeep.com>
2018-11-06 12:56:13 -0500

    dirty commit - work on rx refactor

--- RhymeGame/RhymingWordsViewController.swift
+++ RhymeGame/RhymingWordsViewController.swift
@@ -1,18 +1,15 @@
 import UIKit
+import RxSwift
 
 class RhymingWordsViewController: UIViewController, UITextFieldDelegate {
 
     //MARK: Lifecycle overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadNextRhymePair()
+        bindUItoViewModel()
+        viewModel.getNextRhymePair()
         rhymeOneTextField.delegate = self
         rhymeTwoTextField.delegate = self
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        updateUI()
     }
 
     //MARK: Private properies
@@ -22,16 +19,15 @@ class RhymingWordsViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private weak var rhymeTwoTextField: UITextField!
     @IBOutlet private weak var continueButton: UIButton!
     @IBOutlet private weak var scoreLabel: UILabel!
+    @IBOutlet private weak var leftStackView: UIStackView!
+    @IBOutlet private weak var rightStackView: UIStackView!
 
-    private var rhymePair: RhymeWordPair? {
-        didSet {
-            updateUI()
-        }
-    }
+    let viewModel = ViewModel()
+    let disposeBag = DisposeBag()
 
     private var score: Int = 0 {
         didSet {
-            updateUI()
+            scoreLabel.text = "Score: \(score)"
         }
     }
 
@@ -40,7 +36,7 @@ class RhymingWordsViewController: UIViewController, UITextFieldDelegate {
         if userGuessIsCorrect() {
             let alertVC = UIAlertController(title: "Correct!", message: "", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                self.loadNextRhymePair()
+                self.viewModel.getNextRhymePair()
                 self.score += 1
             }))
             present(alertVC, animated: true, completion: nil)
@@ -51,40 +47,61 @@ class RhymingWordsViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
-    private func updateUI() {
-        phraseWordOneLabel.text = rhymePair?.firstRhymeInfo.definitionWithoutRhyme
-        phraseWordTwoLabel.text = rhymePair?.secondRhymeInfo.definitionWithoutRhyme
-        scoreLabel.text = "Current Streak: \(score)"
+    private func bindUItoViewModel() {
+        viewModel.rhymePair.observeOn(MainScheduler.instance)
+                            .subscribe(onNext: { [weak self] pair in self?.updateUI(with: pair) })
+                            .disposed(by: disposeBag)
+
+        viewModel.guessStatus.observeOn(MainScheduler.instance)
+                             .subscribe(onNext: { [weak self] status in self?.updateUI(with: status) } )
+                             .disposed(by: disposeBag)
+    }
+
+    private func updateUI(with rhymePair: RhymeWordPair?) {
+        guard let rhymePair = rhymePair else { spin(); return }
+        stopSpin()
+        phraseWordOneLabel.text = rhymePair.firstRhymeInfo.definitionWithoutRhyme
+        phraseWordTwoLabel.text = rhymePair.secondRhymeInfo.definitionWithoutRhyme
         rhymeOneTextField.text = ""
         rhymeTwoTextField.text = ""
+    }
+
+    private func updateUI(with guessStatus: ViewModel.GuessStatus) {
+        let leftColor: UIColor
+        let rightColor: UIColor
+        switch guessStatus {
+        case .noneCorrect:
+            leftColor = .red
+            rightColor = .red
+        case .leftCorrect:
+            rightColor = .red
+            leftColor = .green
+        case .rightCorrect:
+            rightColor = .green
+            leftColor = .red
+        case .bothCorrect:
+            leftColor = .green
+            rightColor = .green
+        }
+        leftStackView.backgroundColor = leftColor
+        rightStackView.backgroundColor = rightColor
+    }
+
+    private func spin() {
+        print("loading...")
+    }
+
+    private func stopSpin() {
+        print("done loading")
     }
 
     private func userGuessIsCorrect() -> Bool {
         let rhymeOne = rhymeOneTextField.text
         let rhymeTwo = rhymeTwoTextField.text
-        return rhymeOne?.lowercased() == rhymePair?.firstRhymeInfo.rhyme.lowercased() && rhymeTwo?.lowercased() == rhymePair?.secondRhymeInfo.rhyme.lowercased()
+        return true
+        //return rhymeOne?.lowercased() == rhymePair?.firstRhymeInfo.rhyme.lowercased() && rhymeTwo?.lowercased() == rhymePair?.secondRhymeInfo.rhyme.lowercased()
     }
 
-    private func loadNextRhymePair() {
-        WordnikAPIClient.manager.getNextRhymePair { [weak self] (pair, error) in
-            DispatchQueue.main.async {
-                self?.rhymePair = pair
-                //To do: Persist
-                print(pair.debugDescription)
-                print(error.debugDescription)
-                if let error = error as? WordnikError {
-                    switch error {
-                    case .noValidRymes, .badDefinition: self?.loadNextRhymePair()
-                    }
-                }
-                if let error = error as? JSONError {
-                    //To do: Handle this better
-                    print(error)
-                    self?.loadNextRhymePair()
-                }
-            }
-        }
-    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         validateAnswer(in: textField)
         textField.resignFirstResponder()
@@ -96,8 +113,66 @@ class RhymingWordsViewController: UIViewController, UITextFieldDelegate {
 }
 
 extension RhymingWordsViewController {
-    struct ViewModel {
+    class ViewModel {
+        enum GuessStatus {
+            case noneCorrect
+            case leftCorrect
+            case rightCorrect
+            case bothCorrect
+        }
 
+        var rhymePair: Observable<RhymeWordPair?> { return rhymePairSubject.asObservable() }
+        var guessStatus: Observable<GuessStatus> { return guessStatusSubject.asObservable() }
+
+        private var currentRhymePair: RhymeWordPair? {
+            didSet {
+                self.rhymePairSubject.onNext(currentRhymePair)
+            }
+        }
+
+        func getNextRhymePair() {
+            WordnikAPIClient.manager.getNextRhymePair { [weak self] (pair, error) in
+                self?.handle(error: error, pair: pair)
+            }
+        }
+
+        func checkAnswers(leftGuess: String, rightGuess: String) -> GuessStatus {
+            guard let pair = currentRhymePair else { return .noneCorrect }
+
+            let leftRhyme = pair.firstRhymeInfo.rhyme
+            let righthRhyme = pair.secondRhymeInfo.rhyme
+
+            switch (leftGuess == leftRhyme, rightGuess == righthRhyme) {
+            case (false, false): return .noneCorrect
+            case (false, true): return .rightCorrect
+            case (true, false): return .leftCorrect
+            case (true, true): return .bothCorrect
+            }
+        }
+
+        private func handle(error: Error?, pair: RhymeWordPair?) {
+            if let error = error as? WordnikError {
+                switch error {
+                case .badDefinition, .noValidRymes: self.getNextRhymePair()
+                }
+            }
+            if let error = error as? JSONError {
+                switch error {
+                case .noDefinition, .noPhraseInfo: self.getNextRhymePair()
+                case .noData: self.getNextRhymePair() //Fallback on saved pair?
+                case .parseError: fatalError("Developer Error: Unable to parse JSON")
+                }
+            }
+            self.currentRhymePair = pair
+        }
+
+        lazy private var rhymePairSubject: BehaviorSubject<RhymeWordPair?> = {
+            return BehaviorSubject(value: nil)
+        }()
+
+        lazy private var guessStatusSubject: BehaviorSubject<GuessStatus> = {
+            return BehaviorSubject(value: .noneCorrect)
+        }()
     }
 }
 
